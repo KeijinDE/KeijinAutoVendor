9613d6dde2544f15dd0bcb14770919dafe17069b
Keijin <keijin.der.suchti@gmail.com>
2025-06-20 23:10:19 +0200

    Aktualisieren von auto_vendor_repair.lua

--- auto_vendor_repair.lua
+++ auto_vendor_repair.lua
@@ -1,20 +1,20 @@
-local function KAV_FormatCopper(copper)
-  local gold = math.floor(copper / (100 * 100))
-  local silver = math.floor((copper - (gold * 100 * 100)) / 100)
-  local copperOnly = math.fmod(copper, 100)
-  return string.format("|cffffff00%dg|r |cffc7c7cf%ds|r |cffeda55f%dc|r", gold, silver, copperOnly)
-end
+-- auto_vendor_repair.lua
 
 function KAV_HandleRepair()
-  if CanMerchantRepair() then
-    local cost, canRepair = GetRepairAllCost()
-    if canRepair and cost > 0 then
-      RepairAllItems()
-      DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r Repaired gear for " .. KAV_FormatCopper(cost))
-    elseif KAV_DEBUG then
+  local cost, canRepair = GetRepairAllCost()
+
+  if KAV_DEBUG then
+    DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Repair check: cost=" .. tostring(cost) .. ", canRepair=" .. tostring(canRepair))
+  end
+
+  if cost and cost > 0 and canRepair then
+    RepairAllItems()
+    if KAV_DEBUG then
+      DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Repaired items for " .. GetCoinTextureString(cost))
+    end
+  else
+    if KAV_DEBUG then
       DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Nothing to repair.")
     end
-  elseif KAV_DEBUG then
-    DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Merchant cannot repair.")
   end
 end
