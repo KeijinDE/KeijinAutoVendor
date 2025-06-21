67516d6bc84e24fd6fc0a6b33b0bbf46d426ff46
Keijin <keijin.der.suchti@gmail.com>
2025-06-21 16:23:18 +0200

    Update auto_vendor_repair.lua

--- auto_vendor_repair.lua
+++ auto_vendor_repair.lua
@@ -1,20 +1,39 @@
 -- auto_vendor_repair.lua
 
-function KAV_HandleRepair()
-  local cost, canRepair = GetRepairAllCost()
+local function Print(msg)
+  DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r " .. msg)
+end
 
-  if KAV_DEBUG then
-    DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Repair check: cost=" .. tostring(cost) .. ", canRepair=" .. tostring(canRepair))
+local function Debug(msg)
+  if KAV_DebugMode then
+    DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons][Debug]|r " .. msg)
+  end
+end
+
+local function SafeCoinString(amount)
+  if type(GetCoinTextureString) == "function" then
+    return GetCoinTextureString(amount)
+  else
+    return tostring(amount) .. " copper"
+  end
+end
+
+function KAV_HandleRepair()
+  local canRepair = CanMerchantRepair()
+  local repairCost = 0
+
+  if type(GetRepairAllCost) == "function" then
+    local cost, possible = GetRepairAllCost()
+    repairCost = cost or 0
+    canRepair = canRepair and possible
   end
 
-  if cost and cost > 0 and canRepair then
+  Debug("Repair check. cost=" .. tostring(repairCost) .. ", canRepair=" .. tostring(canRepair))
+
+  if canRepair and repairCost > 0 then
     RepairAllItems()
-    if KAV_DEBUG then
-      DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Repaired items for " .. GetCoinTextureString(cost))
-    end
-  else
-    if KAV_DEBUG then
-      DEFAULT_CHAT_FRAME:AddMessage("|cff88ff88[KeijinAddons]|r [Debug] Nothing to repair.")
-    end
+    Debug("Repaired gear for " .. SafeCoinString(repairCost))
+  elseif canRepair then
+    Debug("Nothing to repair.")
   end
 end
